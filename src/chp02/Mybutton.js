cfff444dd234517b0295126ab9c53edf992c10a5
hyunhyesoo <sue990831@gmail.com>
2025-08-26 16:22:39 +0900

    리액트 2,3장

---
+++ src/chp02/Mybutton.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+function Mybutton(props) {
+    const [isClicked, setIsClicked] = React.useState(false);
+
+    return React.createElement(
+        'button',
+        {onClick:() => setIsClicked(true)},
+        isClicked ? 'Clicked' : 'Click Here'
+    )
+}
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(Mybutton));
